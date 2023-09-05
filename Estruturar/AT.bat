e7e5b827f5a5e49e2cdbc7eb96e48015042f1f3d
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2023-09-05 08:47:59 -0300

    Update AT.bat

--- Estruturar/AT.bat
+++ Estruturar/AT.bat
@@ -1,8 +1,8 @@
 ﻿@ECHO OFF
-curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/02-Identificacao.bat
-curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/03-Plano_de_Fundo.bat
-curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/04-Conexao.bat
-curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/05-Programas.bat
-curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/06-Bloqueios.bat
-curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/07-LanSchool.bat
-curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/08-Particao.bat
+@curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/02-Identificacao.bat > nul 2>&1
+@curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/03-Plano_de_Fundo.bat > nul 2>&1
+@curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/04-Conexao.bat > nul 2>&1
+@curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/05-Programas.bat > nul 2>&1
+@curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/06-Bloqueios.bat > nul 2>&1
+@curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/07-LanSchool.bat > nul 2>&1
+@curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/08-Particao.bat > nul 2>&1
