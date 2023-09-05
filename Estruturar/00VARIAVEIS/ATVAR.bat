5a385aab8689da3e4f8faf2775b978dbe27b0746
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2023-09-05 00:54:59 -0300

    Update ATVAR.bat

--- Estruturar/00VARIAVEIS/ATVAR.bat
+++ Estruturar/00VARIAVEIS/ATVAR.bat
@@ -1,1 +1,1 @@
-curl -LJO "https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/00VARIAVEIS/varRede.bat"
+curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/00VARIAVEIS/varRede.bat
