b4e097150df218e49634e9b43cd50d5bb23ab139
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2023-09-05 00:46:56 -0300

    Update AT.bat

--- Estruturar/AT.bat
+++ Estruturar/AT.bat
@@ -4,3 +4,4 @@ curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/04-Co
 curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/05-Programas.bat
 curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/06-Bloqueios.bat
 curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/07-LanSchool.bat
+curl -LJO https://alexstocco-senai.github.io/ConfiguracoesSENAI/Estruturar/08-Particao.bat
