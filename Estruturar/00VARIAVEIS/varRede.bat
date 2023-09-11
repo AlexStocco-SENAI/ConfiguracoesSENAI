ad690a5d084b4c006c6135723aea886d2767fa15
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2023-09-11 16:52:22 -0300

    Update varRede.bat

--- Estruturar/00VARIAVEIS/varRede.bat
+++ Estruturar/00VARIAVEIS/varRede.bat
@@ -3,8 +3,8 @@ IF %vAmbienteLocal%==A04 (
           SET vMaquinaVLAN=LABORATORIO_CNC
           SET vMaquinaGateway=10.110.136.1
           SET vMaquinaMascara=255.255.255.128
-          SET vMaquinaDNS1=10.190.254.2
-          SET vMaquinaDNS2=10.190.254.3
+          SET vMaquinaDNS1=10.110.144.5
+          SET vMaquinaDNS2=10.110.144.6
           IF %vMaquinaIdentificacao%==00 SET vMaquinaIP=10.110.136.2
           IF %vMaquinaIdentificacao%==01 SET vMaquinaIP=10.110.136.10
           IF %vMaquinaIdentificacao%==02 SET vMaquinaIP=10.110.136.11
