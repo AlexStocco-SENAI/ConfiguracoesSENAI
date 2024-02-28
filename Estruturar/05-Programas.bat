9ae2a026520583b21a50fcd8622ffefe23b9076b
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2024-02-28 13:05:52 -0300

    Update 05-Programas.bat

--- Estruturar/05-Programas.bat
+++ Estruturar/05-Programas.bat
@@ -5,36 +5,36 @@ IF "%vStart%"=="1" (
     ECHO ABRINDO PROGRAMAS
 
     REM AUTOCAD 2024
-    start " " "C:\Arquivos de Programas\Autodesk\AutoCAD 2024\acad.exe"  /product ACAD /language "en-US"
+    start " " "C:\Arquivos de Programas\Autodesk\AutoCAD 2024\acad.exe"  /product ACAD /language "en-US" > nul 2>&1
 
     REM INVENTOR 2024
-    start " " "C:\Arquivos de Programas\Autodesk\Inventor 2024\Bin\Inventor.exe" /language=ENU
+    start " " "C:\Arquivos de Programas\Autodesk\Inventor 2024\Bin\Inventor.exe" /language=ENU > nul 2>&1
 
     REM REVIT 2024
-    start " " "C:\Arquivos de Programas\Autodesk\Revit 2024\Revit.exe" /language ENU
+    start " " "C:\Arquivos de Programas\Autodesk\Revit 2024\Revit.exe" /language ENU > nul 2>&1
 
     REM FUSION
-    rem start " " "C:\Program Files\Autodesk\webdeploy\production\6a0c9611291d45bb9226980209917c3d\FusionLauncher.exe"
+    start " " "C:\Program Files\Autodesk\webdeploy\production\6a0c9611291d45bb9226980209917c3d\FusionLauncher.exe" > nul 2>&1
 
     REM WORD
     start Winword.exe
     echo NFY9XKV6D4RJJ83T9QT8D9P | clip
 
     REM INDUSOFT
-    REM start " " "C:\Program Files (x86)\Indusoft Web Studio Educational v8.1\Bin\RunUniReg.exe"
-    REM start " " "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://www.indusoft.com/license-activation-commercial
+    start " " "C:\Program Files (x86)\Indusoft Web Studio Educational v8.1\Bin\RunUniReg.exe"  > nul 2>&1
+    start " " "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://www.indusoft.com/license-activation-commercial  > nul 2>&1
 
     REM VISUAL STUDIO
-    REM start " " "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+    REM start " " "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"  > nul 2>&1
 
     REM SSCNC
-    start " " "C:\Nanjing Swansoft\SSCNC\CNCLogin.exe"
+    start " " "C:\Nanjing Swansoft\SSCNC\CNCLogin.exe"  > nul 2>&1
 
     REM EDGECAM
-    start " " "C:\Program Files\Hexagon\EDGECAM 2022.1\cam\Launcher.exe"
+    start " " "C:\Program Files\Hexagon\EDGECAM 2022.1\cam\Launcher.exe"  > nul 2>&1
 
     REM PRONEST
-    start " " "C:\Program Files\Hypertherm CAM\ProNest 2023\ProNest.exe"
+    start " " "C:\Program Files\Hypertherm CAM\ProNest 2023\ProNest.exe"  > nul 2>&1
 
     REM TIA Portal
     start " " "C:\Program Files\Siemens\Automation\Automation License Manager\almapp\almapp64x.exe"
