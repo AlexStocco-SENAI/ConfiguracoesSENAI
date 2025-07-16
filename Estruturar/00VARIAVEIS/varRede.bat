d4d4c22c0d2a17db879aa8c1ba6ce8b8f756ba0c
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2025-07-16 15:59:35 -0300

    Update varRede.bat

--- Estruturar/00VARIAVEIS/varRede.bat
+++ Estruturar/00VARIAVEIS/varRede.bat
@@ -1,13 +1,19 @@
 REM V3.10
 @ECHO OFF
+REM DNS PADRÃO
+SET vMaquinaDNS3=10.110.144.5
+SET vMaquinaDNS4=10.110.144.6
+SET vMaquinaDNS1=10.190.240.78
+SET vMaquinaDNS2=10.190.240.79
+
 IF %vAmbienteLocal%==A04 (
           SET vMaquinaVLAN=LABORATORIO_CNC
           SET vMaquinaGateway=10.110.136.1
           SET vMaquinaMascara=255.255.255.128
-          SET vMaquinaDNS3=10.110.144.5
-          SET vMaquinaDNS4=10.110.144.6
-          SET vMaquinaDNS1=10.190.240.78
-          SET vMaquinaDNS2=10.190.240.79
+          REM SET vMaquinaDNS1=10.110.144.5
+          REM SET vMaquinaDNS2=10.110.144.6
+          REM SET vMaquinaDNS3=10.190.240.78
+          REM SET vMaquinaDNS4=10.190.240.79
           IF %vMaquinaIdentificacao%==00 SET vMaquinaIP=10.110.136.2
           IF %vMaquinaIdentificacao%==01 SET vMaquinaIP=10.110.136.10
           IF %vMaquinaIdentificacao%==02 SET vMaquinaIP=10.110.136.11
@@ -36,10 +42,10 @@ IF %vAmbienteLocal%==AS03 (
           SET vMaquinaVLAN=LABORATORIO_CNC
           SET vMaquinaGateway=10.110.136.1
           SET vMaquinaMascara=255.255.255.128
-          SET vMaquinaDNS3=10.110.144.5
-          SET vMaquinaDNS4=10.110.144.6
-          SET vMaquinaDNS1=10.190.240.78
-          SET vMaquinaDNS2=10.190.240.79
+          REM SET vMaquinaDNS1=10.110.144.5
+          REM SET vMaquinaDNS2=10.110.144.6
+          REM SET vMaquinaDNS3=10.190.240.78
+          REM SET vMaquinaDNS4=10.190.240.79
           IF %vMaquinaIdentificacao%==00 SET vMaquinaIP=10.110.136.3
           IF %vMaquinaIdentificacao%==01 SET vMaquinaIP=10.110.136.41
           IF %vMaquinaIdentificacao%==02 SET vMaquinaIP=10.110.136.42
@@ -67,10 +73,10 @@ IF %vAmbienteLocal%==AS05 (
           SET vMaquinaVLAN=LAB_INFORMATICA_2
           SET vMaquinaGateway=10.110.134.129
           SET vMaquinaMascara=255.255.255.128
-          SET vMaquinaDNS1=10.110.144.5
-          SET vMaquinaDNS2=10.110.144.6
-          SET vMaquinaDNS3=10.190.240.78
-          SET vMaquinaDNS4=10.190.240.79
+          REM SET vMaquinaDNS1=10.110.144.5
+          REM SET vMaquinaDNS2=10.110.144.6
+          REM SET vMaquinaDNS3=10.190.240.78
+          REM SET vMaquinaDNS4=10.190.240.79
           IF %vMaquinaIdentificacao%==00 SET vMaquinaIP=10.110.134.130
           IF %vMaquinaIdentificacao%==01 SET vMaquinaIP=10.110.134.140
           IF %vMaquinaIdentificacao%==02 SET vMaquinaIP=10.110.134.141
@@ -99,10 +105,10 @@ IF %vAmbienteLocal%==AS07 (
           SET vMaquinaVLAN=LAB_PNEUMATICA
           SET vMaquinaGateway=10.110.135.1
           SET vMaquinaMascara=255.255.255.128
-          SET vMaquinaDNS1=10.110.144.5
-          SET vMaquinaDNS2=10.110.144.6
-          SET vMaquinaDNS3=10.190.240.78
-          SET vMaquinaDNS4=10.190.240.79
+          REM SET vMaquinaDNS1=10.110.144.5
+          REM SET vMaquinaDNS2=10.110.144.6
+          REM SET vMaquinaDNS3=10.190.240.78
+          REM SET vMaquinaDNS4=10.190.240.79
           IF %vMaquinaIdentificacao%==00 SET vMaquinaIP=10.110.135.2
           IF %vMaquinaIdentificacao%==01 SET vMaquinaIP=10.110.135.11
           IF %vMaquinaIdentificacao%==02 SET vMaquinaIP=10.110.134.12
@@ -131,10 +137,10 @@ IF %vAmbienteLocal%==AS08 (
           SET vMaquinaVLAN=LAB_PNEUMATICA
           SET vMaquinaGateway=10.110.135.1
           SET vMaquinaMascara=255.255.255.128
-          SET vMaquinaDNS1=10.110.144.5
-          SET vMaquinaDNS2=10.110.144.6
-          SET vMaquinaDNS3=10.190.240.78
-          SET vMaquinaDNS4=10.190.240.79
+          REM SET vMaquinaDNS1=10.110.144.5
+          REM SET vMaquinaDNS2=10.110.144.6
+          REM SET vMaquinaDNS3=10.190.240.78
+          REM SET vMaquinaDNS4=10.190.240.79
           IF %vMaquinaIdentificacao%==00 SET vMaquinaIP=10.110.135.3
           IF %vMaquinaIdentificacao%==01 SET vMaquinaIP=10.110.135.41
           IF %vMaquinaIdentificacao%==02 SET vMaquinaIP=10.110.134.42
