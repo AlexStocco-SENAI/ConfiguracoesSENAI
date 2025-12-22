2cf92c8e772013098ea8c66d8983558a52037f95
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2025-12-22 16:36:17 -0300

    Add files via upload

---
+++ W11/09-Ativacao_Intune.bat
@@ -0,0 +1,30 @@
+@echo off
+echo Elevando usuario SENAI para ADMIN...
+
+net localgroup Administradores SENAI /add
+
+if %errorlevel%==0 (
+    echo Usuario SENAI agora e ADMINISTRADOR.
+) else (
+    echo ERRO ao alterar o usuario SENAI.
+)
+
+
+REM HABILITANDO ATUALIZAÇÃO
+
+"C:\Program Files\NoVirusThanks\Win Update Stop\WinUpdateStop.exe"  /enableupdates
+
+
+echo ================================
+echo  INICIANDO ATUALIZACAO WINDOWS
+echo ================================
+echo.
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) { ^
+    Install-Module PSWindowsUpdate -Force -Scope AllUsers ^
+} ^
+Import-Module PSWindowsUpdate; ^
+Install-WindowsUpdate -AcceptAll -AutoReboot"
+
+
