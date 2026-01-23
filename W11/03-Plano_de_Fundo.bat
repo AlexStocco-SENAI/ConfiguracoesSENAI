9db2c754b33269d45f240be563c3c9ad41d03bb0
Alex Stocco <alexstocco.senai@gmail.com>
2026-01-23 10:16:31 -0300

    Plano de fundo W11

--- W11/03-Plano_de_Fundo.bat
+++ W11/03-Plano_de_Fundo.bat
@@ -2,6 +2,7 @@ REM V3.01
 ECHO "ALTERANDO PLANO DE FUNDO"
 
 copy "C:\Padronizacao\Midia\Planos de fundo\Plano de Fundo (%vMaquinaIdentificacao%).JPG" "C:\Padronizacao\Midia\Planos de fundo\Plano de Fundo.JPG" /y
+copy "C:\Padronizacao\Midia\Planos de fundo\Plano de Fundo.JPG" "C:\Windows\Web\Wallpaper\Windows\img0.jpg" /y
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
 
