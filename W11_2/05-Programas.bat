ea4c466f6257c7ff806a0dcf4c234c733de1e73e
AlexStocco-SENAI <103438000+AlexStocco-SENAI@users.noreply.github.com>
2026-01-23 13:11:37 -0300

    Refactor program launch script for efficiency
    Updated program launching script to a more concise version that uses a loop for program execution. Added special handling for Word and Indusoft.

--- W11_2/05-Programas.bat
+++ W11_2/05-Programas.bat
@@ -1,141 +1,56 @@
-REM V3.04
-REM INICIA PROGRAMAS QUE NECESSITAM DE ATIVAÇÃO DAS LICENÇAS
+@echo off
+REM V3.06
 
+IF NOT "%vStart%"=="1" GOTO :EOF
 
 ECHO ABRINDO PROGRAMAS
 
-REM AUTOCAD
-    set "program1=C:\Arquivos de Programas\Autodesk\AutoCAD 2024\acad.exe" 
-    IF "%vStart%"=="1" (
-        if exist "%program1%" (
-            start "" "%program1%"
-        )
-    )
-    set "program1=C:\Arquivos de Programas\Autodesk\AutoCAD 2025\acad.exe" 
-    IF "%vStart%"=="1" (
-        if exist "%program1%" (
-            start "" "%program1%"
-        )
-    )
+echo bJR4vC | clip
 
-    set "program1=C:\Arquivos de Programas\Autodesk\AutoCAD 2026\acad.exe" 
-    IF "%vStart%"=="1" (
-        if exist "%program1%" (
-            start "" "%program1%"
-        )
-    )
+REM ===============================
+REM LISTA DE PROGRAMAS
+REM ===============================
 
-REM INVENTOR
-    set "program2=C:\Arquivos de Programas\Autodesk\Inventor 2026\Bin\Inventor.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program2%" (
-            start "" "%program2%"  /language=ENU
-        )
+for %%P in (
+    "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+    "C:\Arquivos de Programas\Autodesk\AutoCAD 2024\acad.exe"
+    "C:\Arquivos de Programas\Autodesk\AutoCAD 2025\acad.exe"
+    "C:\Arquivos de Programas\Autodesk\AutoCAD 2026\acad.exe"
+    "C:\Arquivos de Programas\Autodesk\Inventor 2026\Bin\Inventor.exe"
+    "C:\Arquivos de Programas\Autodesk\Inventor 2024\Bin\Inventor.exe"
+    "C:\Arquivos de Programas\Autodesk\Inventor 2022\Bin\Inventor.exe"
+    "C:\Arquivos de Programas\Autodesk\Revit 2026\Revit.exe"
+    "C:\Arquivos de Programas\Autodesk\Revit 2025\Revit.exe"
+    "C:\Arquivos de Programas\Autodesk\Revit 2024\Revit.exe"
+    "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+    "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\devenv.exe"
+    "C:\Nanjing Swansoft\SSCNC\CNCLogin.exe"
+    "C:\Program Files\Hexagon\EDGECAM 2022.1\cam\Launcher.exe"
+    "C:\Program Files\Hypertherm CAM\ProNest 2023\ProNest.exe"
+    "C:\Program Files\Siemens\Automation\Automation License Manager\almapp\almapp64x.exe"
+    "C:\Program Files (x86)\Didactic\FluidSIM6\bin\FluidSIM6.exe"
+) do (
+    if exist "%%~P" (
+        echo Abrindo %%~P
+        start "" "%%~P"
     )
-    set "program2=C:\Arquivos de Programas\Autodesk\Inventor 2024\Bin\Inventor.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program2%" (
-            start "" "%program2%"  /language=ENU
-        )
-    )
-    set "program2=C:\Arquivos de Programas\Autodesk\Inventor 2022\Bin\Inventor.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program2%" (
-            start "" "%program2%"  /language=ENU
-        )
-    )
+)
 
-REM REVIT
-    set "program=C:\Arquivos de Programas\Autodesk\Revit 2026\Revit.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"  /language ENU
-        )
-    )
-    set "program=C:\Arquivos de Programas\Autodesk\Revit 2024\Revit.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"  /language ENU
-        )
-    )
-
-    set "program=C:\Arquivos de Programas\Autodesk\Revit 2025\Revit.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"  /language ENU
-        )
-    )
-
-REM FUSION
-    set "program=C:\Program Files\Autodesk\webdeploy\production\6a0c9611291d45bb9226980209917c3d\FusionLauncher.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"
-        )
-    )
+REM ===============================
+REM PROGRAMAS ESPECIAIS
+REM ===============================
 
 REM WORD
-    IF "%vStart%"=="1" (
-        start Winword.exe
-        echo laboratorio_cfp603@senaisp.edu.br | clip
-    )
-
+start Winword.exe
+echo laboratorio_cfp603@senaisp.edu.br | clip
 
 REM INDUSOFT
-    set "program3=C:\Program Files (x86)\Indusoft Web Studio Educational v8.1\Bin\RunUniReg.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program3%" (
-            start "" "%program3%"
-            REM start " " "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://www.indusoft.com/license-activation-commercial
-            start " " "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://om.aveva.com/InduSoftActivation/
-        )
-    )
-    
+set "indusoft=C:\Program Files (x86)\Indusoft Web Studio Educational v8.1\Bin\RunUniReg.exe"
+if exist "%indusoft%" (
+    start "" "%indusoft%"
+    start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://om.aveva.com/InduSoftActivation/
+)
 
-REM VISUAL STUDIO
-    set "program=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"
-        )
-    )
 
-REM SSCNC
-    set "program=C:\Nanjing Swansoft\SSCNC\CNCLogin.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"
-        )
-    )
 
-REM EDGECAM
-    set "program=C:\Program Files\Hexagon\EDGECAM 2022.1\cam\Launcher.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"
-        )
-    )
-
-REM PRONEST
-    set "program=C:\Program Files\Hypertherm CAM\ProNest 2023\ProNest.exe" 
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"
-        )
-    )
-
-REM TIA Portal
-    set "program=C:\Program Files\Siemens\Automation\Automation License Manager\almapp\almapp64x.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"
-        )
-    )
-
-REM FluidSim
-    set "program=C:\Program Files (x86)\Didactic\FluidSIM6\bin\FluidSIM6.exe"
-    IF "%vStart%"=="1" (
-        if exist "%program%" (
-            start "" "%program%"
-        )
-    )
